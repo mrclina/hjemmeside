d28cfe8e18a51b16d53cd431f16fea714091b395
Marcelina Dylewska <m.dylewska@student.uib.no>
2025-09-17 18:02:05 +0200

    nettside

---
+++ src/app/prosjekter/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/header";
+
+export default function Prosjekt() {
+    return (
+        <div>
+            <h1>Hei fra prosjekter</h1>
+        </div>
+)
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="flex justify-between p-6 border-b mb-4">
+            <a href="/">Hjem</a>
+            <a href="/prosjekter">Prosjekter</a>
+        </div>
+    )
+}
